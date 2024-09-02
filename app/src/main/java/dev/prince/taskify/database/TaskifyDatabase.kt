6bd61643e7a55ffca9433c07df9fceca1eb35914
devprincefahad <1pfchouhan@gmail.com>
2024-09-03 01:01:02 +0530

    hilt setup

--- app/src/main/java/dev/prince/taskify/database/TaskifyDatabase.kt
+++ app/src/main/java/dev/prince/taskify/database/TaskifyDatabase.kt
@@ -4,6 +4,6 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(entities = [Task::class], version = 1, exportSchema = false)
-abstract class TaskDatabase : RoomDatabase() {
+abstract class TaskifyDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
 }
---
+++ app/src/main/java/dev/prince/taskify/di/AppModule.kt
@@ -0,0 +1,30 @@
+package dev.prince.taskify.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import dev.prince.taskify.database.TaskifyDatabase
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRoomInstance(@ApplicationContext context: Context) = Room.databaseBuilder(
+        context,
+        TaskifyDatabase::class.java,
+        "taskify_database"
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideTaskDao(db: TaskifyDatabase) = db.taskDao()
+
+}
