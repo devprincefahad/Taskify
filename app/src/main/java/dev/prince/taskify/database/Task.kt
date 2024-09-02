a40033c5c81486ab9170d83436edb4d7cff58a9e
devprincefahad <1pfchouhan@gmail.com>
2024-09-03 00:58:40 +0530

    room db setup

---
+++ app/src/main/java/dev/prince/taskify/database/Task.kt
@@ -0,0 +1,14 @@
+package dev.prince.taskify.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class Task(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val description: String? = null,
+    val isCompleted: Boolean = false,
+    val dueDate: Long? = null
+)
