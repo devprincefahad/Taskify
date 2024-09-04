096cc35bcb8bd9217f8c6740d7b2f5756218194e
devprincefahad <1pfchouhan@gmail.com>
2024-09-04 23:17:00 +0530

    destination fix

--- app/src/main/java/dev/prince/taskify/ui/home/HomeScreen.kt
+++ app/src/main/java/dev/prince/taskify/ui/home/HomeScreen.kt
@@ -6,35 +6,24 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -46,10 +35,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onPlaced
-import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
@@ -66,7 +52,7 @@ import dev.prince.taskify.R
 import dev.prince.taskify.signin.GoogleAuthUiClient
 import dev.prince.taskify.ui.components.AddTaskBottomSheet
 import dev.prince.taskify.ui.components.TaskList
-import dev.prince.taskify.ui.destinations.SingInScreenDestination
+import dev.prince.taskify.ui.destinations.SignInScreenDestination
 import dev.prince.taskify.ui.theme.poppinsFamily
 import dev.prince.taskify.util.LocalSnackbar
 import kotlinx.coroutines.delay
@@ -104,7 +90,7 @@ fun HomeScreen(
     }
 
     if (googleAuthUiClient.getSignedInUser() == null) {
-        navigator.navigate(SingInScreenDestination)
+        navigator.navigate(SignInScreenDestination)
     }
 
     val pagerState = rememberPagerState(pageCount = { 2 })
